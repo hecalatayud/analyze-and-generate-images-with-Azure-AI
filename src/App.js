d0ab0897effeec860f3048f4ac5be2fa7d77382e
hecalatayud <hecalatayud@gmail.com>
2023-11-14 23:49:53 +0100

    Added application GUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+// Add the necessary code in function App to build a page with a Title named "Computer Vision", a text input with prompt "Enter URL to analyze or textual prompt to generate an image" and two buttons below with will be "Analyze" and "Generate".
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Computer Vision</h1>
+        <p>
+          Enter URL to analyze or textual prompt to generate an image
+        </p>
+        <input type="text" id="input" />
+        <button id="analyze" onClick={() => {}}>Analyze</button>
+        <button id="generate" onClick={() => {}}>Generate</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
